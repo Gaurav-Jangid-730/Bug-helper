e11f244d4811758b225c77fc4022f0988065c108
Gaurav Sharma <gauravjangid7300@gmail.com>
2025-03-07 22:13:57 +0530

    Fix

--- subdomain_Enumatration/Virustotal.py
+++ subdomain_Enumatration/Virustotal.py
@@ -1,30 +1,37 @@
 import requests
-import json
 
-def get_subdomains(domain, api_key):
-    url = f"https://www.virustotal.com/api/v3/domains/{domain}"
+def get_subdomains(target_dir,domain, api_key):
+    filename = f"{target_dir}/{domain}_subdomains.txt"
+    BASE_URL = f"https://www.virustotal.com/api/v3/domains/{DOMAIN}/subdomains?limit=40"
     headers = {
         "accept": "application/json",
         "x-apikey": api_key
     }
-    
-    response = requests.get(url, headers=headers)
-    
-    if response.status_code == 200:
-        data = response.json()
-        subdomains = data.get("data", {}).get("attributes", {}).get("subdomains", [])
-        
-        if subdomains:
-            filename = f"{domain}_subdomains.txt"
-            with open(filename, "w") as file:
-                for subdomain in subdomains:
-                    file.write(subdomain + "\n")
-            print(f"Subdomains saved to {filename}")
-        else:
-            print("No subdomains found.")
-    else:
-        print(f"Error: {response.status_code} - {response.text}")
 
-# Example usage:
-# Replace 'your_api_key_here' with your actual VirusTotal API key
-#get_subdomains("google.com", "your_api_key_here")
+    subdomains = []
+    cursor = None
+
+    while True:
+        url = BASE_URL
+        if cursor:
+            url = cursor
+        
+        response = requests.get(url, headers=headers)
+        if response.status_code != 200:
+            print("Error:", response.json())
+            break
+
+        data = response.json()
+        
+        # Extract subdomains
+        for entry in data.get("data", []):
+            subdomains.append(entry.get("id"))
+
+        # Get next cursor if available
+        cursor = data.get("links",{}).get("next")
+        if not cursor:
+            break
+    with open(filename, "w") as file:
+        for subdomain in subdomains:
+            file.write(subdomain + "\n")
+    print(f"Subdomains saved to {filename}")
