e4ee8383bb707f256aa9765b13a56709f7c0e6d1
Gaurav Sharma <gauravjangid7300@gmail.com>
2025-03-07 20:42:04 +0530

    Fix

--- subdomain_Enumatration/subdomain_finder.py
+++ subdomain_Enumatration/subdomain_finder.py
@@ -10,7 +10,8 @@ def subdomain_finding(target, target_dir, enable_bruteforce):
         f"mv {target}.txt {target_dir}/findomain-{target}.txt",
     ]
     virustotal = False
-    file_path = "subdomain_Enumatration/virustotal_api.txt"
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(script_dir, "subdomain_Enumatration", "virustotal_api.txt")
     if os.path.exists(file_path):
         virustotal = True
         with open(file_path, "r") as file:
